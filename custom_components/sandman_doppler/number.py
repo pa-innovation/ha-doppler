1701dbf0f8840ebfb58a9fee790b55d7f5c6f3eb
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-07-19 23:27:57 +0000

    number.py: changed set_native_value to async and tried an await on the api call

--- custom_components/sandman_doppler/number.py
+++ custom_components/sandman_doppler/number.py
@@ -43,9 +43,9 @@ class DopplerVolumeLevelNumber(DopplerEntity, NumberEntity):
         """Return the current value."""
         return self.coordinator.data[self.device.name][ATTR_VOLUME_LEVEL]
 
-    def set_native_value(self, value) -> None:
+    async def set_native_value(self, value) -> None:
         """Update the current volume value"""
         self._attr_native_value = value
-        self.coordinator.api.set_volume_level(self.device,value)
+        await self.coordinator.api.set_volume_level(self.device,value)
 
 
