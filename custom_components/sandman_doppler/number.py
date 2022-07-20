6ee7ff4dcb4a2f84a50101d9cd9663cdd9dfc448
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-07-20 18:29:58 +0000

    number.py: Changed set_native_value back to async_set_native_value

--- custom_components/sandman_doppler/number.py
+++ custom_components/sandman_doppler/number.py
@@ -49,7 +49,7 @@ class DopplerVolumeLevelNumber(DopplerEntity, NumberEntity):
         """Return the current value"""
         return self.coordinator.data[self.device.name][ATTR_VOLUME_LEVEL]
     
-    def set_native_value(self, value:float) -> None:
+    async def async_set_native_value(self, value:float) -> None:
         """Update the current volume value"""
         self._attr_native_value = value
         await self.coordinator.api.set_volume_level(self.device,int(value))
