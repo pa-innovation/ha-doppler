e4d12922e1ac0e3b7942d0b311f01b755e0b4a3d
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-07-20 23:51:28 +0000

    __init__.py: Added set_sync_button_display_brightness to async_setup_entry

--- custom_components/sandman_doppler/__init__.py
+++ custom_components/sandman_doppler/__init__.py
@@ -73,9 +73,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     mydevices= await client.get_devices();
     for device in mydevices.values():
-        await client.set_sync_button_display_color(device,False)
-        await client.set_sync_day_night_color(device,False)
-
+        await client.set_sync_button_display_color(device, False)
+        await client.set_sync_day_night_color(device, False)
+        await client.set_sync_button_display_brightness(device, False)
     
     return True
 
