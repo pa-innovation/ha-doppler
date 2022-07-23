f5a112274f6afc94f6791f50dcd7e0b77671ef6e
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-07-23 01:00:16 +0000

    switch.py:Tried updating the hass attributes from switch

--- custom_components/sandman_doppler/switch.py
+++ custom_components/sandman_doppler/switch.py
@@ -35,13 +35,13 @@ class ColonBlinkSwitch(DopplerEntity, SwitchEntity):
 
     async def async_turn_on(self, **kwargs):
         """Turn Colon Blinking On"""
-        await self.coordinator.api.set_colon_blink_mode(
+        device_data[ATTR_COLON_BLINK]=await self.coordinator.api.set_colon_blink_mode(
             self.device, True)
 
 
     async def async_turn_off(self, **kwargs):
         """Turn Colon Blink Off"""
-        await self.coordinator.api.set_colon_blink_mode(
+        device_data[ATTR_COLON_BLINK]=await self.coordinator.api.set_colon_blink_mode(
             self.device, False)
         
 
