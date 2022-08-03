55b0c99e402c355157a142ff2d95efb949b771a9
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-08-03 20:42:19 +0000

    __init__.py: Added a log warning to iteration of devices to look at id's

--- custom_components/sandman_doppler/__init__.py
+++ custom_components/sandman_doppler/__init__.py
@@ -91,6 +91,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     mydevices= await client.get_devices()
     for device in mydevices.values():
+        _LOGGER.warning(f"printing id {device.id}")
         await client.set_sync_button_display_color(device, False)
         await client.set_sync_day_night_color(device, False)
         await client.set_sync_button_display_brightness(device, False)
