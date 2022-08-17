da16d72be96de8923df8c5c0d324c6db27789ead
Cameron Kellough <cameron@paloaltoinnovation.com>
2022-08-17 22:52:00 +0000

    device_trigger.py:  After changing a lot of random code, this one works.

--- custom_components/sandman_doppler/device_trigger.py
+++ custom_components/sandman_doppler/device_trigger.py
@@ -15,13 +15,11 @@ from homeassistant.components.automation import (
 from homeassistant.components.device_automation import DEVICE_TRIGGER_BASE_SCHEMA
 from homeassistant.components.homeassistant.triggers import event as event_trigger
 from homeassistant.const import (
-    CONF_EVENT,
     CONF_DEVICE_ID,
     CONF_DOMAIN,
     CONF_ENTITY_ID,
     CONF_PLATFORM,
     CONF_TYPE,
-    CONF_SUBTYPE,
     STATE_OFF,
     STATE_ON,
 )
@@ -30,22 +28,13 @@ from homeassistant.helpers import config_validation as cv, entity_registry
 from homeassistant.helpers import device_registry as dr
 from homeassistant.helpers.typing import ConfigType
 
-from .const import (
-    DOMAIN,
-    SANDMAN_DOPPLER_BUTTON1_EVENT,
-    SANDMAN_DOPPLER_BUTTON2_EVENT,
-    ATTR_DSN,
-    ATTR_BUTTON,
-)
+from .const import DOMAIN
 
 TRIGGER_TYPES = {"sandman_doppler_button_event"}
 
 TRIGGER_SCHEMA = DEVICE_TRIGGER_BASE_SCHEMA.extend(
     {
         vol.Required(CONF_TYPE): vol.In(TRIGGER_TYPES),
-        vol.Required(ATTR_DSN): cv.string,
-        vol.Required(ATTR_BUTTON): vol.Coerce(int),
-        vol.Required(CONF_SUBTYPE): vol.Coerce(str),
     }
 )
 
@@ -54,45 +43,45 @@ async def async_get_triggers(
     hass: HomeAssistant, device_id: str
 ) -> list[dict[str, Any]]:
     """List device triggers for sandman_doppler devices."""
+    registry = entity_registry.async_get(hass)
     device_registry = dr.async_get(hass)
-
     triggers = []
 
     _LOGGER.warning(f"device_id={device_id}")
 
-    device_entry = device_registry.async_get(device_id)
+    device_entry = device_registry.async_get(str(device_id))
     assert device_entry
     _LOGGER.warning(f"device_entry.identifiers={device_entry.identifiers}")
 
     # for id in device_entry.identifiers:
     #     if id[1].startswith("Doppler"):
     #         dsn = id[1]
-    dsn = "test"
+#    dsn = "test"
 
-    for i in range(1, 3):
-        triggers.append(
-            {
-                CONF_PLATFORM: "device",
-                CONF_DOMAIN: "sandman_doppler",
-                CONF_DEVICE_ID: device_id,
-                ATTR_DSN: dsn,
-                ATTR_BUTTON: i,
-                CONF_TYPE: "sandman_doppler_button_event",
-                CONF_SUBTYPE: i,
-            }
-        )
+#    for i in range(1, 3):
+#        triggers.append(
+#            {
+#                CONF_PLATFORM: "device",
+#                CONF_DOMAIN: "sandman_doppler",
+#                CONF_DEVICE_ID: device_id,
+#                ATTR_DSN: dsn,
+#                ATTR_BUTTON: i,
+#                CONF_TYPE: "Doppler-4fcd8f3c_butt_1",
+#                CONF_SUBTYPE: i,
+#            }
+#        )
 
-    _LOGGER.warning(f"triggers= {triggers}")
+#    _LOGGER.warning(f"triggers= {triggers}")
 
     return [
         {
             CONF_PLATFORM: "device",
             CONF_DOMAIN: DOMAIN,
             CONF_DEVICE_ID: device_id,
-            ATTR_DSN: dsn,
-            ATTR_BUTTON: 1,
+#            ATTR_DSN: dsn,
+#            ATTR_BUTTON: 1,
             CONF_TYPE: "sandman_doppler_button_event",
-            CONF_SUBTYPE: "1",
+#            CONF_SUBTYPE: "1",
         }
     ]
 
@@ -112,8 +101,7 @@ async def async_attach_trigger(
     #        if id.startswith("Doppler"):
     #            dsn=id
 
-    event_config = event_trigger.TRIGGER_SCHEMA(
-        {
+    event_config = event_trigger.TRIGGER_SCHEMA({
             event_trigger.CONF_PLATFORM: "event",
             event_trigger.CONF_EVENT_TYPE: "sandman_doppler_button_event",
             #        event_trigger.CONF_EVENT_DATA: {
@@ -122,8 +110,7 @@ async def async_attach_trigger(
             #            CONF_DEVICE_ID: config[CONF_DEVICE_ID],
             #            CONF_TYPE: config[CONF_TYPE],
             #        },
-        }
-    )
+        })
 
     _LOGGER.warning(f"event_config={event_config}")
 
